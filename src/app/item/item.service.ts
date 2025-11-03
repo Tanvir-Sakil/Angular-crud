9567408eee670805e91aee674b3055137f675658
Tanvir-Sakil <tanbirshakil18@gmail.com>
2025-11-03 10:25:13 +0600

    Initial commit

---
+++ src/app/item/item.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Item } from './item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+
+  constructor(private _httpClient:HttpClient) { }
+
+  private baseUrl = "https://localhost:7119/items";
+
+  getAllItem()
+  {
+    return this._httpClient.get<Item[]>(this.baseUrl);
+  }
+  create(data:Item)
+  {
+    return this._httpClient.post(`${this.baseUrl}`, data);
+  }
+   getById(id:Number)
+  {
+    return this._httpClient.get<Item>(`${this.baseUrl}/${id}`);
+  }
+   update(data:Item)
+  {
+    return this._httpClient.put<Item>(`${this.baseUrl}/${data.id}`,data);
+  }
+  delete(id:number)
+  {
+    return this._httpClient.delete<Item>(`${this.baseUrl}/${id}`);
+  }
+}
